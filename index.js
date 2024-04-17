984c0e8ebee17b36b9b22360067bdebd439c07ea
techphosis <techphosis175041@gmail.com>
2024-04-17 13:58:08 +0530

    add

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+const PORT=8000;
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+
+app.get('/', (request, response) => {
+  return response.send('welcome to Docker project');
+});
+
+//for all wrong path
+app.all('*', function (req, res) {
+  try {
+      return res.status(200).send(`Wrong url path to Docker project`);
+  } catch (error) {
+      return res.status(400).send("some went wrong" + error);
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT : ${PORT}`);
+});
